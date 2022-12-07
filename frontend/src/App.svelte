da560ebb8321175c55c5d844d3102e7348f028eb
just-Jana <117265367+just-Jana@users.noreply.github.com>
2022-12-07 13:58:40 +0100

    added simple error page

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,15 @@
 <script>
     import { Router, Route } from "svelte-routing"
     import Demo from "./routes/demo.svelte"
+    import Error from "./routes/error.svelte"
 
     let url=""
 </script>
 
 <Router url="{url}">
     <main>
-      <!--<Route path="/example" component="{Example}" />-->
+      <!--<Route path="/example" component={Example} />-->
+      <Route path="/error" component={Error} />
       <Route path="/"><Demo /></Route>
     </main>
 </Router>
