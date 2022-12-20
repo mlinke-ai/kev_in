1582d4b064412fa7854f2a2484b3291511904671
Max Linke <max.linke@live.de>
2022-12-20 16:34:35 +0100

    Added --clean flag

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import shutil
 
 from backend.server import Server
 
@@ -11,8 +12,12 @@ def main():
         prog="Kev.in Management Script", description="The control script to manage the server."
     )
     parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("-c", "--clean", action="store_true")
     parser.add_argument("--host", action="store_true")
     args = parser.parse_args()
+
+    if args.clean:
+        shutil.rmtree("instance")
 
     server = Server()
     server.run(args.debug, args.host)
