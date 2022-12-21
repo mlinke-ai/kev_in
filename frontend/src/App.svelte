d32750e6a195f043bd91f0eb0bdc7e32e22bc3d1
pymon <simonguenther02@gmail.com>
2022-12-21 16:39:50 +0100

    Exchanged Router in App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,26 +1,19 @@
 <script>
-    import { Router, Route } from "svelte-routing"
-    import Error from "./routes/error.svelte"
+  import Router from "svelte-spa-router";
+  import routes from "./routes";
+
   import Navbar from "./lib/components/Navbar/Navbar.svelte";
-  import Login from "./routes/login.svelte";
-  import Home from "./routes/home.svelte";
 
   let url = "";
 </script>
 
-<Router {url}>
-  <Navbar />
-  <main>
-    <Route path="/auth" component={Login} />
-    <!--<Route path="/example" component="{Example}" />-->
-    <Route path="/"><Home /></Route>
-    <Route path="/error" component={Error} />
-  </main>
-</Router>
+<Navbar />
+<main>
+  <Router {routes} />
+</main>
 
 <style>
   main {
     margin-top: 5rem;
   }
 </style>
-
