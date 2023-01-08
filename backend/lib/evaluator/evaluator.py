0d2f855621f8914edeb89bc124260952944a4638
Max Linke <max.linke@live.de>
2023-01-08 15:15:08 +0100

    Added evaluator function place holders

--- backend/lib/evaluator/evaluator.py
+++ backend/lib/evaluator/evaluator.py
@@ -8,6 +8,14 @@ from sandboxes.javaenv.javasandbox import ExecuteJava
 class Evaluator:
     @classmethod
     def execute(cls, code, language):
+        # TODO: add execution of code
+        # execute code inside the language respective sandbox
+        pass
+
+    @classmethod
+    def evaluate(cls, exercise, solution):
+        # TODO: add evaluation of solution
+        # evaluate a solution against a an exercise
         pass
 
 
