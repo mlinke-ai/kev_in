da703583ab2cfeb4f8b177df61dd42154adc6d7f
Constantin <c.stumm@gmx.de>
2023-01-15 14:23:34 +0100

    Improve style and implement getExercise

--- fill_DB.py
+++ fill_DB.py
@@ -6,16 +6,27 @@ r = requests.request("POST", "http://127.0.0.1:5000/user", json={"user_name": "t
 print(r.status_code)
 
 # sign in
-r = requests.request("POST", "http://127.0.0.1:5000/login", json={"user_name": "test_user", "user_pass": "12341234"}, headers={"Content-Type": "application/json"})
+r = requests.request("POST", "http://127.0.0.1:5000/login", json={
+                     "user_name": "sadmin", "user_pass": "sadmin"}, headers={"Content-Type": "application/json"})
 token = r.cookies.get("token")
 print(r.status_code)
 
 # create an ParsonsPuzzleExercise
 r = requests.request(
-            "POST",
-            "http://127.0.0.1:5000/exercise",
-            json={"exercise_title": "Solve the Puzzle", "exercise_description":
-                  "Place the items in the correct order!", "exercise_type": 3, "exercise_content": "1-print(123) 2-return0"},
-            headers={"Content-Type": "application/json", "Cookie": "token="+token}, 
-        )
+  "POST",
+  "http://127.0.0.1:5000/exercise",
+  json={"exercise_title": "Solve the Puzzle", "exercise_description":
+        "Place the items in the correct order!", "exercise_type": 3, "exercise_content": "1-print(123) 2-return0"},
+  headers={"Content-Type": "application/json",
+            "Cookie": "token="+token},
+)
 print(r.status_code)
+
+# retrieve an ParsonsPuzzleExercise -> works only with the exercise id =(
+r = requests.request(
+  "GET",
+  "http://127.0.0.1:5000/exercise",
+  json= {"exercise_title": "Solve the Puzzle"},
+  headers={"Content-Type": "application/json", "Cookie": "token="+token})
+
+print(r.status_code, r.content)
--- frontend/src/lib/components/ParsonsPuzzleExercise/PuzzleCard.svelte
+++ frontend/src/lib/components/ParsonsPuzzleExercise/PuzzleCard.svelte
@@ -4,18 +4,8 @@
 
   let initHeight = 0;
 
-  let itemsLeft = [
-    {id: 1, name: "print(22 + 66 + sum([1,2,3]))"},
-    {id: 2, name: "print(22)\n34"},
-    {id: 3, name: "item3"},
-    {id: 4, name: "item3"},
-    {id: 5, name: "item3"},
-    {id: 6, name: "item3"},
-    {id: 7, name: "item3"},
-    {id: 8, name: "item3"},
-    {id: 9, name: "item4"}
-  ];
-  let itemsRight = [];
+  export let itemsLeft;
+  export let itemsRight;
   let myTestVAR;
   console.log(myTestVAR)
 </script>
@@ -24,10 +14,10 @@
 <UiCard icon="extension" title="Puzzle">
   <div class="lists-container" bind:clientHeight={myTestVAR}>
     <div class="list">
-      <HorizontalList bind:items={itemsLeft} height={((myTestVAR)-100).toString()}/>
+      <HorizontalList bind:items={itemsLeft} height={((myTestVAR)-(myTestVAR)*0.125).toString()}/>
     </div>
     <div class="list">
-      <HorizontalList bind:items={itemsRight} height={((myTestVAR)-100).toString()}/>
+      <HorizontalList bind:items={itemsRight} height={((myTestVAR)-(myTestVAR)*0.125).toString()}/>
     </div>
   </div>
 </UiCard>
@@ -39,7 +29,6 @@
     flex-direction: row;
     justify-content: center;
     height: 100%;
-    align-items: center;
   }
   .list{
     flex: 1;
