c31eec473d6f5933fcf47cbdd2399de613362f60
Constantin <c.stumm@gmx.de>
2023-01-15 01:56:46 +0100

    Adapt PPE to Exercise design

---
+++ frontend/src/lib/components/ParsonsPuzzleExercise/PuzzleCard.svelte
@@ -0,0 +1,48 @@
+<script>
+  import HorizontalList from "../horizontalList.svelte";
+  import UiCard from "../common/UICard.svelte";
+
+  let initHeight = 0;
+
+  let itemsLeft = [
+    {id: 1, name: "print(22 + 66 + sum([1,2,3]))"},
+    {id: 2, name: "print(22)\n34"},
+    {id: 3, name: "item3"},
+    {id: 4, name: "item3"},
+    {id: 5, name: "item3"},
+    {id: 6, name: "item3"},
+    {id: 7, name: "item3"},
+    {id: 8, name: "item3"},
+    {id: 9, name: "item4"}
+  ];
+  let itemsRight = [];
+  let myTestVAR;
+  console.log(myTestVAR)
+</script>
+
+
+<UiCard icon="extension" title="Puzzle">
+  <div class="lists-container" bind:clientHeight={myTestVAR}>
+    <div class="list">
+      <HorizontalList bind:items={itemsLeft} height={((myTestVAR)-100).toString()}/>
+    </div>
+    <div class="list">
+      <HorizontalList bind:items={itemsRight} height={((myTestVAR)-100).toString()}/>
+    </div>
+  </div>
+</UiCard>
+
+<style>
+  .lists-container{
+    margin: 1rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    height: 100%;
+    align-items: center;
+  }
+  .list{
+    flex: 1;
+  }
+
+</style>
--- frontend/src/routes/ParsonsPuzzleExercise.svelte
+++ frontend/src/routes/ParsonsPuzzleExercise.svelte
@@ -1,19 +1,14 @@
 <script>
     import {flip} from "svelte/animate";
     import {dndzone} from "svelte-dnd-action";
-    import HorizontalList from "../lib/components/horizontalList.svelte";
     import Button from "@smui/button/src/Button.svelte";
-    let itemsLeft = [
-        {id: 1, name: "print(22 + 66 + sum([1,2,3]))"},
-        {id: 2, name: "print(22)\n34"},
-        {id: 3, name: "item3"},
-        {id: 4, name: "item4"}
-    ];
-    let itemsRight = [];
+    import Page from "../lib/components/common/Page.svelte";
+    import { Icon } from "@smui/common";
+    import PuzzleCard from "../lib/components/ParsonsPuzzleExercise/PuzzleCard.svelte";
 
-    function submit(){
-        console.log(itemsRight)
-    }
+    import TestCard from "../lib/components/CodeSandbox/TestCard.svelte";
+    import CodingCard from "../lib/components/CodeSandbox/CodingCard.svelte";
+    import OutputCard from "../lib/components/CodeSandbox/OutputCard.svelte";
 
     const getExercise = async () =>{
         fetch("/exercise?exercise_id=1", {method: "GET", headers: {"Content-Type": "application/json", "Cookie": "token=<token>"}})
@@ -22,33 +17,80 @@
 
 </script>
 
-
-<div style="margin-top: 200px;">
-    <div class="lists-container">
-        <div class="list">
-            <h2 align="center">Drag your elements</h2>
-            <HorizontalList bind:items={itemsLeft} height="300"/>
+<Page title="Parsons Puzzle Exercise" fullwidth={true}>
+    <div class="exercise-container">
+      <div class="header-area">
+        <h3>Parsons Puzzle Exercise</h3>
+      </div>
+      <div class="task-area">
+        <TestCard/>
+      </div>
+      <div class="puzzle-area">
+        <PuzzleCard/>
+      </div>
+      <div class="status-bar">
+        Getting Started - Attempt 1
+        <div class="clock-widget">
+          00:00
+          <Icon class="material-icons">access_time</Icon>
         </div>
-        <div class="list">
-            <h2 align="center">Drop your Elements here</h2>
-            <HorizontalList bind:items={itemsRight} height="300"/>
-        </div>
+      </div>
     </div>
-</div>
+  </Page>
 
 
-<Button color="primary" variant="raised">Reset</Button>
-<Button color="primary" variant="raised" on:click={submit}>Submit</Button>
-<style>
-    
-    .lists-container{
-        margin-top: 100px;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
+
+<style lang="scss">
+  @use "../variables" as vars;
+
+  .exercise-container {
+    display: grid;
+    grid-template-columns: 3fr 9fr;
+    grid-template-rows: 1.5fr 8fr 0.5fr;
+    grid-template-areas:
+    "head head"
+    "task puzzle"
+    "status status";
+    gap: 1%;
+    padding: 1rem 1rem 0.5rem 1rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: black;
+    // margin-left: 3%;
+  }
+
+  .header-area {
+    grid-area: head;
+    h3 {
+      color: vars.$primaryDark;
     }
-    .list{
-        flex: 1;
+  }
+  .task-area {
+    grid-area: task;
+    overflow: auto;
+  }
+  .puzzle-area {
+    grid-area: puzzle;
+    display: flex;
+    height: 600px;
+  }
+  .status-bar {
+    grid-area: status;
+    color: vars.$consoleColor;
+    font-family: "Roboto Mono";
+    display: flex;
+    align-items: center;
+    :last-child {
+      margin-left: auto;
     }
+  }
+  .clock-widget {
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+  }
 </style>
 
