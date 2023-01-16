1682ce0990c0be2d08fa0e7259beec27695a943c
Constantin <c.stumm@gmx.de>
2023-01-17 00:37:44 +0100

    Implement submit function

--- fill_DB.py
+++ fill_DB.py
@@ -5,7 +5,7 @@ r = requests.request("POST", "http://127.0.0.1:5000/user", json={"user_name": "t
                      "user_mail": "test@example.com", "user_role": 3}, headers={"Content-Type": "application/json"})
 print(r.status_code)
 
-# sign in
+# sign in as admin
 r = requests.request("POST", "http://127.0.0.1:5000/login", json={
                      "user_name": "sadmin", "user_pass": "sadmin"}, headers={"Content-Type": "application/json"})
 token = r.cookies.get("token")
@@ -25,8 +25,7 @@ print(r.status_code)
 # retrieve an ParsonsPuzzleExercise -> works only with the exercise id =(
 r = requests.request(
   "GET",
-  "http://127.0.0.1:5000/exercise",
-  json= {"exercise_title": "Solve the Puzzle"},
+  "http://127.0.0.1:5000/exercise?exercise_type=3",
   headers={"Content-Type": "application/json", "Cookie": "token="+token})
 
 print(r.status_code, r.content)
