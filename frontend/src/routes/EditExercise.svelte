e124a7324e78e8094007923e57e5f37a378db8c0
Andreas <andreas.schadli@tuc-racing.de>
2023-01-30 20:09:12 +0100

    Initial commit for edit exercise page

---
+++ frontend/src/routes/EditExercise.svelte
@@ -0,0 +1,96 @@
+<script>
+    import Page from "../lib/components/common/Page.svelte";
+    import Tab, { Label } from "@smui/tab";
+    import TabBar from "@smui/tab-bar";
+    import Textfield from "@smui/textfield";
+    import HelperText from "@smui/textfield/helper-text";
+    import Button from "@smui/button";
+
+    let active = "Sandbox";
+
+    let title = "";
+    let description = "";
+    let functionSignature = "";
+    let test = "";
+</script>
+
+<Page>
+    <div>
+        <!--
+          Note: tabs must be unique. (They cannot === each other.)
+        -->
+        <TabBar
+            tabs={["Sandbox", "Parsons Puzzle", "Gap Text"]}
+            let:tab
+            bind:active
+        >
+            <!-- Note: the `tab` property is required! -->
+            <Tab {tab}>
+                <Label>{tab}</Label>
+            </Tab>
+        </TabBar>
+
+        <div>
+            {#if active == "Sandbox"}
+                <div class="forms">
+                    <Textfield
+                        bind:value={title}
+                        variant="outlined"
+                        helperLine$style="width: 100%;"
+                        class="textfields"
+                    >
+                        <HelperText persistent slot="helper">Title</HelperText>
+                    </Textfield>
+
+                    <Textfield
+                        bind:value={description}
+                        variant="outlined"
+                        textarea
+                        helperLine$style="width: 100%;"
+                        class="textfields"
+                    >
+                        <HelperText persistent slot="helper"
+                            >Description</HelperText
+                        >
+                    </Textfield>
+
+                    <Textfield
+                        bind:value={functionSignature}
+                        variant="outlined"
+                        helperLine$style="width: 100%;"
+                        class="textfields"
+                    >
+                        <HelperText persistent slot="helper">Function Signature</HelperText>
+                    </Textfield>
+
+                    <Textfield
+                    bind:value={test}
+                    variant="outlined"
+                    textarea
+                    helperLine$style="width: 100%;"
+                    class="textfields"
+                >
+                    <HelperText persistent slot="helper"
+                        >Test</HelperText
+                    >
+                </Textfield>
+
+                <Button on:click={()=>true} variant="raised" style="margin-top: 3em;">
+                    <Label>Submit</Label>
+                  </Button>
+                </div>
+            {:else if active == "Parsons Puzzle"}
+                <h1>PARSONS PUZZLE</h1>
+            {:else}
+                <h1>GAP TEXT</h1>
+            {/if}
+        </div>
+    </div>
+</Page>
+
+<style>
+    * :global(.textfields)  {
+        margin-top: 2em;
+        width: 100%;
+    }
+</style>
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,12 +2,14 @@ import Home from "./Home.svelte"
 import Profile from "./Profile.svelte"
 import Error from "./Error.svelte"
 import CodeSandbox from "./CodeSandbox.svelte"
+import EditExercise from "./EditExercise.svelte"
 //import ExamplePage from "./Example.svelte"
 
 export default {
     "/": Home,
     "/profile": Profile,
     "/sandbox": CodeSandbox,
+    "/edit-exercise": EditExercise,
     //"/example-path": <ExamplePage>,
     "*": Error
 }
