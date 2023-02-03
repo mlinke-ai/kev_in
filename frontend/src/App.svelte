30e1c4cbcb2a538f78b6df9f1c446db23318c5b2
Bananandi <bananandi.dev@gmail.com>
2023-02-04 00:57:26 +0100

    Created first version of the cookie acception banner

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,6 +4,8 @@
   import routes from "./routes/";
   import { setupUserSettings } from "./lib/functions/user";
   import { accessLevel } from "./stores";
+  import Footer from "./lib/components/Footer/Footer.svelte";
+  import AcceptCookies from "./lib/components/AcceptCookies/AcceptCookies.svelte";
 
   function prepareApp() {
     setupUserSettings();
@@ -19,5 +21,6 @@
 {#if $accessLevel >= 0}
   <Navbar />
   <Router {routes} />
-  <!--<Footer />-->
+  <AcceptCookies />
+  <Footer />
 {/if}
---
+++ frontend/src/lib/components/AcceptCookies/AcceptCookies.svelte
@@ -0,0 +1,68 @@
+<script>
+  import Button from "@smui/button";
+  import Banner, { Label } from "@smui/banner";
+
+  let open = true;
+</script>
+
+{#if open}
+  <div id="blur" />
+  <div id="banner">
+    <Banner
+      id="banner"
+      open
+      fixed
+      mobileStacked
+      content$style="max-width: max-content;"
+    >
+      <Label slot="label">
+        <div id="message">
+          <p>
+            This website uses cookies to provide all functionalities.
+            <br />
+            By using this site you consent to these cookies!
+          </p>
+        </div>
+      </Label>
+      <Button id="button" slot="actions" on:click={() => (open = false)}
+        >Okay</Button
+      >
+    </Banner>
+  </div>
+{/if}
+
+<style>
+  #blur {
+    position: fixed;
+    top: 0%;
+    left: 0%;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(5px);
+  }
+
+  #banner {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+
+    width: 420px;
+    height: 120px;
+    margin-top: -60px;
+    margin-left: -210px;
+
+    background-color: rgba(0, 20, 17, 1);
+    border-radius: 20px;
+  }
+
+  #message {
+    padding-left: 15px;
+  }
+
+  * :global(#button) {
+    left: 65%;
+    color: white;
+    background-color: rgba(0, 57, 49, 1);
+  }
+</style>
