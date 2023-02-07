99806a8ee8cf1d0aa085cf15406f34f6996383cb
Andreas <andreas.schadli@tuc-racing.de>
2023-02-07 17:54:41 +0100

    Changed the layout of the cookie banner. Implemented a flag in local storage to remember that the cookies have been accepted.

--- frontend/src/lib/components/AcceptCookies/AcceptCookies.svelte
+++ frontend/src/lib/components/AcceptCookies/AcceptCookies.svelte
@@ -1,21 +1,24 @@
 <script>
   import Button from "@smui/button";
-  import Banner, { Label } from "@smui/banner";
 
-  let open = true;
+  var open;
+  const cookiesAccepted = localStorage.getItem('cookiesAccepted');
+  if (cookiesAccepted == "true") {
+    open = false;
+  } else {
+    open = true;
+  }
+
+  function okClicked(){
+    open = false;
+    localStorage.setItem('cookiesAccepted', 'true');
+  }
+  // let open = true;
 </script>
 
 {#if open}
   <div id="blur" />
   <div id="banner">
-    <Banner
-      id="banner"
-      open
-      fixed
-      mobileStacked
-      content$style="max-width: max-content;"
-    >
-      <Label slot="label">
         <div id="message">
           <p>
             This website uses cookies to provide all functionalities.
@@ -23,46 +26,48 @@
             By using this site you consent to these cookies!
           </p>
         </div>
-      </Label>
-      <Button id="button" slot="actions" on:click={() => (open = false)}
+      <Button id="button" slot="actions" on:click={() => okClicked()}
         >Okay</Button
       >
-    </Banner>
   </div>
 {/if}
 
 <style>
   #blur {
     position: fixed;
-    top: 0%;
-    left: 0%;
-    width: 100%;
-    height: 100%;
+    top: 0vh;
+    left: 0vw;
+    width: 100vw;
+    height: 100vh;
     background-color: rgba(0, 0, 0, 0.5);
     backdrop-filter: blur(5px);
   }
 
   #banner {
     position: fixed;
-    top: 50%;
-    left: 50%;
+    top: 40vh;
+    left: 33vw;
 
-    width: 420px;
-    height: 120px;
-    margin-top: -60px;
-    margin-left: -210px;
+    width: 32vw;
+    height: fit-content;
 
-    background-color: rgba(0, 20, 17, 1);
-    border-radius: 20px;
+    padding-left: 1vw;
+    padding-right: 1vw;
+    padding-bottom: 2vh;
+    padding-top: 2vh;
+    text-align: center;
+
+    background-color: rgba(0,57,49,1);
+    border-radius: 2vw;
   }
 
   #message {
-    padding-left: 15px;
+    text-align: center;
+    font-size: larger;
   }
 
   * :global(#button) {
-    left: 65%;
     color: white;
-    background-color: rgba(0, 57, 49, 1);
+    background-color: #005f50;
   }
 </style>
