02a89e9341d590f6fc3a444cd1c79cc584243084
Bananandi <bananandi.dev@gmail.com>
2023-02-11 01:04:23 +0100

    Created file and cheap example text with blanks for the fill in the blanks exercise.

---
+++ frontend/src/routes/Blanks.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Page from "../lib/components/common/Page.svelte";
+    import Textfield from "@smui/textfield";
+
+</script>
+
+<Page>
+    <h1>Fill in the Blanks</h1>
+   
+    <p>
+        Lorem ipsum dolor <input type="text"> amet, consectetur adipiscing elit.
+        Aliquam magna <input type="text">, pretium ut nibh a, <input type="text"> porttitor nisl.
+        Pellentesque ornare, odio <input type="text"> suscipit eleifend, elit nibh fermentum.
+        Sed auctor sollicitudin dolor, ut tincidunt diam elementum non.
+        Phasellus ut velit at sem <input type="text"> porttitor a eget ante.
+    </p>
+
+</Page>
