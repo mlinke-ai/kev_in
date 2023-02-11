91d8d32ea5370e468eda7571a2dbb025ba4f023c
Bananandi <bananandi.dev@gmail.com>
2023-02-11 01:04:23 +0100

    Created file and cheap example text with blanks for the fill in the blanks exercise.

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,12 +2,14 @@ import Home from "./Home.svelte"
 import Dashboard from "./Dashboard.svelte"
 import Error from "./Error.svelte"
 import CodeSandbox from "./CodeSandbox.svelte"
+import Blanks from "./Blanks.svelte"
 //import ExamplePage from "./Example.svelte"
 
 export default {
     "/": Home,
     "/dashboard": Dashboard,
     "/sandbox": CodeSandbox,
+    "/blanks": Blanks,
     //"/example-path": <ExamplePage>,
     "*": Error
 }
