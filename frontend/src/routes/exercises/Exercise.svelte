2e0ff68871b5d74ba9b667bf86820b4843c0557b
pymon <simonguenther02@gmail.com>
2023-02-14 14:55:33 +0100

    Changed routing structure, added Exercise component

---
+++ frontend/src/routes/exercises/Exercise.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { getExercise } from "../../lib/Excercises/exercise";
+
+    export let params = {};
+    let exerciseID = params.exerciseID;
+
+    console.log(getExercise(exerciseID));
+</script>
