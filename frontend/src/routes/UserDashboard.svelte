507f7a48e4ce7b88dc4e74e162a6f61dce8bc308
just-Jana <117265367+just-Jana@users.noreply.github.com>
2023-02-15 15:22:09 +0100

    improved user-dashboard

--- frontend/src/routes/UserDashboard.svelte
+++ frontend/src/routes/UserDashboard.svelte
@@ -13,7 +13,9 @@
   import GroupSvg from "../lib/AnimatedSVG/GroupSVG.svelte";
   import ExerciseSvg from "../lib/AnimatedSVG/ExerciseSVG.svelte";
   import { userName } from "../stores";
-  //import { userID } from "../stores";
+  import { userID } from "../stores";
+  import SolutionsSvg from "../lib/AnimatedSVG/SolutionsSVG.svelte";
+  import { accessLevels } from "../lib/constants";
 
   //display exercise progress
   let totalExercises = 100;
@@ -24,9 +26,6 @@
   let r = document.querySelector(":root");
   let statsLoaded = false;
 
-  let myName = $userName;
-  //let myID = 1;
-  //later change to $userID
 
   // function myFunction_get() {
   //   let rs = getComputedStyle(r);
@@ -44,7 +43,30 @@
           totalExercises = Object.values(data).length;
           console.log(solvedExercises);
           console.log(totalExercises);
-          //getSolvedExercises(); following function calls might be removed if this is activated
+          getSolvedExercises(); 
+          //following function calls might be removed if this is activated
+          // setTotalExercises();
+          // setSolvedExercises();
+          // setUserProgress();
+          // statsLoaded = true;
+        });
+      } else {
+        alert("Oops an Error occured. " + response.status);
+      }
+    });
+  };
+
+  const getSolvedExercises = async () => {
+    fetch(`/solution?solution_user=${$userID}&solution_correct=true`, {
+      //?solution_user=${$userID}&solution_correct=true
+      //to get the number of all correct solutions of the user with id ${$userID}
+      method: "GET",
+    }).then((response) => {
+      if (response.status === 200) {
+        response.json().then((data) => {
+          statsLoaded = false;
+          console.log(data);
+          solvedExercises = Object.values(data).length;
           setTotalExercises();
           setSolvedExercises();
           setUserProgress();
@@ -55,28 +77,6 @@
       }
     });
   };
-
-  // const getSolvedExercises = async () => {
-  //   fetch(`/solution`, {
-  //     //?solution_user=${myID}&solution_correct=true
-  //     //to get the number of all correct solutions of the user with id ${me}
-  //     method: "GET",
-  //   }).then((response) => {
-  //     if (response.status === 200) {
-  //       response.json().then((data) => {
-  //         statsLoaded = false;
-  //         console.log(data);
-  //         solvedExercises = Object.values(data).length;
-  //         setTotalExercises();
-  //         setSolvedExercises();
-  //         setUserProgress();
-  //         statsLoaded = true;
-  //       });
-  //     } else {
-  //       alert("Oops an Error occured. " + response.status);
-  //     }
-  //   });
-  // };
   //responses error 500
 
   function setUserProgress() {
@@ -90,8 +90,8 @@
   }
 
   function setSolvedExercises() {
-    solvedExercises = totalExercises / 3;
-    //just for testcases
+    solvedExercises = Math.floor(totalExercises / 3);
+    //just for testcases, remove if getSolvedExercises works correctly
     r.style.setProperty("--solvedExercises", solvedExercises + "px");
   }
 
@@ -99,12 +99,12 @@
   //getSolvedExercises();
 </script>
 
-<Page>
+<Page requiredAccessLevel={accessLevels.user}>
   <div class="grid-container-outside">
     <!--  Header -->
     <div class="header-outside">
       <h2 style="padding: 20px; font-family: monospace;">
-        Welcome to your dashboard, {myName}!
+        Welcome to your dashboard, {$userName}!
       </h2>
     </div>
 
@@ -115,20 +115,20 @@
     <div class="main-outside">
       <div class="grid-container-inside">
         <div class="left-inside">
-          <a href="/#/users">
-            <Card>
-              <GroupSvg />
-            </Card>
-            <div class="label">List all users</div>
-          </a>
-        </div>
-
-        <div class="right-inside">
           <a href="/#/exercises">
             <Card>
               <ExerciseSvg />
             </Card>
-            <div class="label">List all exercises</div>
+            <div class="label">list all exercises</div>
+          </a>
+        </div>
+
+        <div class="right-inside">
+          <a href="/#/solutions">
+            <Card>
+              <SolutionsSvg/>
+            </Card>
+            <div class="label">list my solutions</div>
           </a>
         </div>
       </div>
