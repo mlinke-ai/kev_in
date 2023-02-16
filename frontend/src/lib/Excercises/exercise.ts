adf8f736c44962d9421e11e059a5ce01312ab6bb
Pymon <simonguenther02@gmail.com>
2023-02-16 02:21:13 +0100

    Began rewriting parts of the project in typescript

--- frontend/src/lib/Excercises/exercise.ts
+++ frontend/src/lib/Excercises/exercise.ts
@@ -1,8 +1,14 @@
-function getCurrentTimestamp() {
-  return (Date.now() / 1000)
+export interface Exercise {
+  exercise_id: Number;
+  exercise_title: String;
+  exercise_description: String;
+  exercise_type: String; // should be a Number
+  exercise_language: String; // should be a Number
+  exercise_content: Object;
+  exercise_solution: Object;
 }
 
-export const getExercise = async(exerciseID) => {
+export const getExercise = async (exerciseID: number): Promise<Exercise> => {
   try {
     const response = await fetch(
       `/exercise?exercise_id=${exerciseID}&exercise_limit=1&exercise_details=true`,
@@ -10,46 +16,47 @@ export const getExercise = async(exerciseID) => {
         method: "GET",
         headers: { "Content-Type": "application/json" },
       }
-      );
+    );
     if (!response.ok) {
-      console.error(`Could not fetch Exercise #${exerciseID}: HTTP error ${response.status}`);
-      return undefined;
+      throw new Error();
     }
-    return await response.json();
+    return await response.json().then((data) => data[exerciseID]);
+  } catch (error) {
+    throw new Error();
   }
-  catch (error) {
-    console.error(`Could not fetch Exercise #${exerciseID}: ${error}`);
-    return undefined;
-  }
+};
+
+function getCurrentTimestamp() {
+  return Date.now() / 1000;
 }
 
-export const submitSolution = async (exerciseID, elapsedTime, solutionContent) => {
-  fetch(
-    "/solution",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        solution_exercise: exerciseID,
-        solution_date: getCurrentTimestamp(),
-        solution_duration: elapsedTime,
-        solution_content: solutionContent
-      })
-    }
-  ).then(response => {
+export const submitSolution = async (
+  exerciseID,
+  elapsedTime,
+  solutionContent
+) => {
+  fetch("/solution", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      solution_exercise: exerciseID,
+      solution_date: getCurrentTimestamp(),
+      solution_duration: elapsedTime,
+      solution_content: solutionContent,
+    }),
+  }).then((response) => {
     if (response.status === 400) {
       alert("A required argument was not sent");
-    }
-    else if (response.status === 401) {
+    } else if (response.status === 401) {
       // redirect to login
       window.location.replace("/#/login");
     } else if (response.status === 403) {
-      alert("you naughty naughty")
+      alert("you naughty naughty");
     } else if (response.status === 200) {
-      alert("Successfully submitted solution")
-      response.json().then(data => {
+      alert("Successfully submitted solution");
+      response.json().then((data) => {
         console.log(data);
-      })
+      });
     }
-  })
-}
+  });
+};
---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,28 @@
+export enum accessLevels {
+    undefined = -1,
+    default = 0,
+    user = 1,
+    admin = 2,
+    sadmin = 3
+};
+
+export enum messages {
+    neutral = 0,
+    success = 1,
+    warning = 2,
+    error = 3
+}
+
+export enum exerciseTypes {
+    gapText = 1,
+    syntax = 2,
+    parsonsPuzzle = "ParsonsPuzzleExercise",
+    findTheBug = 4,
+    documentation = 5,
+    output = 6,
+    programming = "ProgrammingExercise"
+}
+
+export const passwordLength = 8;
+
+export const dashboardPage = "/admin-dashboard"
