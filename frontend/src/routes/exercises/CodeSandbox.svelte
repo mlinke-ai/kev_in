a3c278a3c3011ba7260b993b9f52a5b8ea6a78b7
Pymon <simonguenther02@gmail.com>
2023-02-16 02:21:13 +0100

    Began rewriting parts of the project in typescript

--- frontend/src/routes/exercises/CodeSandbox.svelte
+++ frontend/src/routes/exercises/CodeSandbox.svelte
@@ -1,45 +1,28 @@
-<script>
+<script lang="ts">
   import Page from "../../lib/common/Page.svelte";
   import TaskCard from "../../lib/Excercises/TaskCard.svelte";
   import CodingCard from "../../lib/Excercises/CodeSandbox/CodingCard.svelte";
   import OutputCard from "../../lib/Excercises/CodeSandbox/OutputCard.svelte";
   import StatusBar from "../../lib/Excercises/StatusBar.svelte";
-  import { getExercise, submitSolution } from "../../lib/Excercises/exercise";
+  import { Exercise, submitSolution } from "../../lib/Excercises/exercise";
 
   let elapsedTime;
-  export let exerciseID = 7
-
-  function get() {
-    console.log(getExercise(exerciseID));
-  }
+  export let exerciseData: Exercise;
 
   function submit() {
-    submitSolution(exerciseID, elapsedTime, "code here");
+    submitSolution(exerciseData.exercise_id, elapsedTime, "code here");
   }
   function reset() {}
-  get();
 </script>
 
 <Page title="Coding Sandbox" fullwidth={true}>
   <div class="sandbox-container">
     <div class="header-area">
-      <h3>Coding Sandbox</h3>
+      <h3>{exerciseData.exercise_title}</h3>
     </div>
     <div class="task-area">
       <TaskCard>
-        <h1>Welcome to your first test exercise!</h1>
-        <p>
-          To solve it you have to write a python script that prints out "Hello
-          World".
-        </p>
-        <br />
-        <hr />
-        <br />
-        Hints:
-        <ul>
-          <li>use the print() function</li>
-          <li>strings have to be in quotation marks</li>
-        </ul>
+        {exerciseData.exercise_description}
       </TaskCard>
     </div>
     <div class="code-area">
--- frontend/src/routes/exercises/Exercise.svelte
+++ frontend/src/routes/exercises/Exercise.svelte
@@ -1,26 +1,23 @@
-<script>
+<script lang="ts">
   import { exerciseTypes } from "../../lib/constants";
-  import { getExercise } from "../../lib/Excercises/exercise";
+  import { Exercise, getExercise } from "../../lib/Excercises/exercise";
   import CodeSandbox from "./CodeSandbox.svelte";
   import ParsonsPuzzleExercise from "./ParsonsPuzzleExercise.svelte";
 
-  export let params = {};
+  export let params: { exerciseID: number };
   let exerciseID = params.exerciseID;
-  let exerciseData;
+  let exerciseData: Exercise;
   let ready = false;
 
   getExercise(exerciseID).then((data) => {
-    if (data) {
-      exerciseData = data[exerciseID];
-      console.log(exerciseData.exercise_type);
-      ready = true;
-    }
+    exerciseData = data;
+    ready = true;
   });
 </script>
 
 {#if ready}
   {#if exerciseData.exercise_type == exerciseTypes.programming}
-    <CodeSandbox />
+    <CodeSandbox {exerciseData}/>
   {:else if exerciseData.exercise_type == exerciseTypes.parsonsPuzzle}
     <ParsonsPuzzleExercise />
   {/if}
