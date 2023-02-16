68645189124cfd6ce63b99df7caa63ae879e155d
Pymon <simonguenther02@gmail.com>
2023-02-16 13:17:23 +0100

    Added more types and fixed issues

--- frontend/src/routes/exercises/ParsonsPuzzleExercise.svelte
+++ frontend/src/routes/exercises/ParsonsPuzzleExercise.svelte
@@ -1,71 +1,61 @@
-<script>
-  import {dndzone} from "svelte-dnd-action";
+<script lang="ts">
   import Page from "../../lib/common/Page.svelte";
   import PuzzleCard from "../../lib/Excercises/ParsonsPuzzle/PuzzleCard.svelte";
-  import { userName } from "../../stores";
-
   import TaskCard from "../../lib/Excercises/TaskCard.svelte";
   import StatusBar from "../../lib/Excercises/StatusBar.svelte";
   import { accessLevels } from "../../lib/constants";
-  import { submitSolution } from "../../lib/Excercises/exercise"
+  import {
+    SolutionPostParsonsPuzzle,
+    submitSolution,
+    getCurrentTimestamp
+  } from "../../lib/Excercises/solution";
+  import type { ParsonsPuzzleExerciseType } from "../../lib/Excercises/exercise";
+
+  export let exerciseData: ParsonsPuzzleExerciseType;
+
+  let solution: SolutionPostParsonsPuzzle;
 
   let itemsLeft = [];
   let itemsLeftOriginal = [];
   let itemsRight = [];
-  let exerciseID = null;
-  let exerciseTitle = "Exercise";
-  let exerciseDescription = "Please use your Brain"
+  let elapsedTime = 0;
 
-  let elapsedTime;
+  itemsRight = [];
+  
+  console.log(exerciseData)
+  let exerciseContent = exerciseData.exercise_content.list;
+  
+  for (let i = 0; i < exerciseContent.length; i++) {
+    itemsLeft[i] = { id: i + 1, name: exerciseContent[i] };
+  }
+  itemsLeftOriginal = itemsLeft.slice();
 
   function submit() {
-    submitSolution(exerciseID, elapsedTime, {list: itemsRight.map(item => item.name)})
+    solution = {
+      solution_exercise: exerciseData.exercise_id,
+      solution_date: getCurrentTimestamp(),
+      solution_duration: elapsedTime,
+      solution_content: {
+        list: ["Hello", "World", "this", "is"]
+      }
+    }
+    submitSolution(solution);
   }
 
-  const getExercise = async () =>{
-    fetch(
-      "/exercise?exercise_type=3&exercise_limit=1&exercise_details=True&exercise_title=hallo", 
-      {
-        method: "GET",
-        headers: {"Content-Type": "application/json"},
-      }
-    ).then(response =>{
-      if (response.status === 401){
-        // redirect to login
-        window.location.replace("/#/login");
-      } else if (response.status === 403){
-        alert("No admin rights")
-      } else if (response.status === 400){
-        alert("user_limit out of range")
-      } else if (response.status === 200){
-        response.json().then(data => {
-          // using static test data for now as data type is not disccused yet
-          itemsRight = [];
-          let exerciseData = Object.values(data)[0]
-          let exerciseContent = exerciseData["exercise_content"]["list"]
-          for(let i = 0; i < exerciseContent.length; i++){
-            itemsLeft[i] = {id: i+1, name: exerciseContent[i]}
-          }
-          itemsLeftOriginal = itemsLeft.slice();
-          exerciseTitle = exerciseData["exercise_title"];
-          exerciseDescription = exerciseData["exercise_description"];
-          exerciseID = exerciseData["exercise_id"]
-        })
-      }
-    })
-  }
-
-  function reset(){
+  function reset() {
     itemsLeft = itemsLeftOriginal.slice();
     itemsRight = [];
-  }  
-  getExercise();
+  }
 </script>
 
-<Page title="Parsons Puzzle Exercise" fullwidth={true} requiredAccessLevel={accessLevels.user}>
+<Page
+  title="Parsons Puzzle Exercise"
+  fullwidth={true}
+  requiredAccessLevel={accessLevels.user}
+>
   <div class="exercise-container">
     <div class="header-area">
-      <h3>{exerciseTitle}</h3>
+      <h3>{exerciseData.exercise_title}</h3>
     </div>
     <div class="task-area">
       <TaskCard />
@@ -73,7 +63,7 @@
     <div class="puzzle-area">
       <PuzzleCard bind:itemsLeft bind:itemsRight />
     </div>
-    <StatusBar {reset} {submit} bind:elapsedTime/>
+    <StatusBar {reset} {submit} bind:elapsedTime />
   </div>
 </Page>
 
