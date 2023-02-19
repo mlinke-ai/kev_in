3d8754eb8c4cf47a82e7d8d3981c53d695d1f6f1
Pymon <simonguenther02@gmail.com>
2023-02-19 23:17:46 +0100

    Optimized code

--- frontend/src/routes/exercises/ExerciseRouter.svelte
+++ frontend/src/routes/exercises/ExerciseRouter.svelte
@@ -1,44 +1,40 @@
 <script lang="ts">
-  import { exercises, messages } from "../../lib/constants";
-  import { ProgrammingExerciseType, getExercise, ParsonsPuzzleExerciseType } from "../../lib/Excercises/exercise";
+  import { exercises } from "../../lib/constants";
+  import {
+    ProgrammingExerciseType,
+    getExercise,
+    ParsonsPuzzleExerciseType,
+  } from "../../lib/Excercises/exercise";
   import ProgrammingExercise from "./ProgrammingExercise.svelte";
   import ParsonsPuzzleExercise from "./ParsonsPuzzleExercise.svelte";
-  import Message from "../../lib/common/Message/Message.svelte";
+  import Error from "../Error.svelte";
+
   export let params: { exerciseID: number };
   let exerciseID = params.exerciseID;
+
   let exerciseData: ProgrammingExerciseType | ParsonsPuzzleExerciseType;
-  let messageComponent;
-  let ready = false;
+  let exerciseComponent;
+  let exercisePromise = get();
 
-  let parsonsPuzzleData: ParsonsPuzzleExerciseType;
-  let programmingData: ProgrammingExerciseType;
-
-  getExercise(exerciseID).then((data) => {
-    exerciseData = data;
-    switch (exerciseData.exercise_type_name) {
-      case exercises.parsonsPuzzle:
-        parsonsPuzzleData = exerciseData as ParsonsPuzzleExerciseType;
-        break
-      case exercises.programming:
-      programmingData = exerciseData as ProgrammingExerciseType;
-        break
-    }
-    ready = true;
-  });
+  async function get() {
+    getExercise(exerciseID).then((data) => {
+      exerciseData = data;
+      switch (exerciseData.exercise_type_name) {
+        case exercises.parsonsPuzzle:
+          exerciseComponent = ParsonsPuzzleExercise;
+          break;
+        case exercises.programming:
+          exerciseComponent = ProgrammingExercise;
+          break;
+      }
+    });
+  }
 </script>
 
-{#if ready}
-  {#if exerciseData.exercise_type_name == exercises.programming}
-    <ProgrammingExercise exerciseData={programmingData} />
-  {:else if exerciseData.exercise_type_name == exercises.parsonsPuzzle}
-    <ParsonsPuzzleExercise exerciseData={parsonsPuzzleData}/>
-  {:else}
-    <Message
-      bind:message={messageComponent}
-      content={`A component for exercise type "${exerciseData.exercise_type_name}" does not
-      exist yet.`}
-      type={messages.error}
-      autoOpen={true}
-    />
-  {/if}
-{/if}
+{#await exercisePromise}
+  Loading exercise...
+{:then _}
+  <svelte:component this={exerciseComponent} {exerciseData} />
+{:catch error}
+  <!-- TODO: Error Handling -->
+{/await}
