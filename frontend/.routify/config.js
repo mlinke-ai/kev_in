c5798e3879459bd7141a655f8a5d960ebf31daf7
pymon <simonguenther02@gmail.com>
2023-02-23 14:29:30 +0100

    Added routify

---
+++ frontend/.routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": true,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "html",
+    "svelte",
+    "md",
+    "svx"
+  ],
+  "started": "2023-02-23T13:20:52.596Z"
+}
