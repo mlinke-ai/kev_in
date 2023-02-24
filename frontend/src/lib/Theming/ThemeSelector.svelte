5ba305e0cb8d35516a8dcdb53c209a86816e08fd
Pymon <simonguenther02@gmail.com>
2023-02-24 10:54:38 +0100

    Added theme selecting menu

--- frontend/src/lib/Theming/ThemeSelector.svelte
+++ frontend/src/lib/Theming/ThemeSelector.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
-    import Button from "@smui/button";
-import { themes, setTheme } from "./themes"
+  import Button from "@smui/button";
+  import { selectedThemeIndex } from "../../stores";
+  import ThemeButton from "./ThemeButton.svelte";
+  import { themes, setTheme } from "./themes";
 
-    
+  function select(index) {
+    if (index != $selectedThemeIndex) {
+      $selectedThemeIndex = index;
+      setTheme(themes[index], true);
+    }
+  }
 </script>
 
-<Button on:click={() => setTheme(themes[1], true)}>Set Theme</Button>
-<Button on:click={() => setTheme(themes[0], true)}>Reset Theme</Button>
+<div class="theme-select-container">
+  {#each themes as theme, index}
+    <ThemeButton
+      on:click={() => select(index)}
+      color={theme.dark["mdc-theme-primary"]}
+      tooltip={theme.name}
+      selected={index === $selectedThemeIndex}
+    />
+  {/each}
+</div>
+
+<style>
+  .theme-select-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    padding-top: .5rem;
+    padding-bottom: .5rem;
+  }
+</style>
