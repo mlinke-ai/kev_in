f845a1ff9c41773956a07221a75c6bb1bcc005d7
Pymon <SimonGuenther02@gmail.com>
2023-02-25 09:38:14 +0100

    Added exercise creation router

---
+++ frontend/src/pages/exercises/create/[exerciseID].svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { exercises } from "../../../lib/Excercises/types";
+  import ParsonsPuzzle from "./parsonspuzzle.svelte";
+  import Programming from "./programming.svelte";
+
+  export let exerciseID: any;
+  exerciseID = exerciseID as unknown as number;
+  let exerciseComponent;
+
+  console.log(exerciseID, exercises.parsonsPuzzle)
+
+  switch (exerciseID) {
+    case <number>exercises.parsonsPuzzle:
+      exerciseComponent = ParsonsPuzzle
+    case <number>exercises.programming:
+    exerciseComponent = Programming
+  }
+</script>
+
+<svelte:component this={exerciseComponent} />
