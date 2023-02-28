0664f6235021a8b821905c0c0b5fd62a59289f4f
Pymon <simonguenther02@gmail.com>
2023-02-28 12:34:19 +0100

    Moved type definitions to component folder

---
+++ frontend/src/lib/Excercises/Programming/types.ts
@@ -0,0 +1,34 @@
+import type { ExerciseGet, ExercisePost, SolutionGet, SolutionPost } from "../types"
+
+export interface ExerciseGetProgramming extends ExerciseGet {
+  exercise_content: {
+    code: string;
+    func: string;
+  };
+  exercise_solution: {
+    key: [params: Array<number>, result: Array<number>];
+  };
+}
+
+export interface ExercisePostProgramming extends ExercisePost {
+  exercise_content: {
+    code: string;
+    func: string;
+  };
+  exercise_solution: {
+    [key: string]: [Array<number>, Array<number>];
+  };
+}
+
+export interface SolutionGetProgramming extends SolutionGet {
+    solution_content: {
+      code: string;
+      func: string;
+    };
+  }
+
+export interface SolutionPostProgramming extends SolutionPost {
+    solution_content: {
+      code: string;
+    };
+  }
