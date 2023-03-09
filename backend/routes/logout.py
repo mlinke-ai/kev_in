a0e41b151c6f62e8b750fcab36d9576cc5aaa7a0
Max Linke <max.linke@live.de>
2023-03-09 15:07:46 +0100

    Removed backend redirect

--- backend/routes/logout.py
+++ backend/routes/logout.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Response, jsonify, make_response, redirect
+from flask import Response, jsonify, make_response
 from flask_jwt_extended import get_jti, verify_jwt_in_request
 from flask_restful import Resource
 from flask_sqlalchemy.query import sqlalchemy
@@ -21,7 +21,7 @@ class LogoutResource(Resource):
             db.session.rollback()
             return make_response(jsonify(dict(message="An error occurred while logging out.")), 500)
         else:
-            response = redirect("/")
+            response = make_response(jsonify(dict()), 200)
             response.delete_cookie("jwt_access_token")
             response.delete_cookie("csrf_access_token")
             return response
